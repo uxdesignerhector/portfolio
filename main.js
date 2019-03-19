60c1ae4f7b2f1563f8a6dee5ebdeadde9d1b20b8
Hector H <uxdesignerhector@gmail.com>
2019-03-19 10:50:29 +0100

    Fixed animated arrow on small screen, deleted nav menu code at main.js

--- main.js
+++ main.js
@@ -13,20 +13,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
-
-
-
-function initNav(){
-  // make drop-downs work properly with touchscreens by preventing instant hover-click
-  $("#home").each(function(){
-    var li = $(this);
-    li.mouseover(function(){
-      // store time of hover event
-      li.data( 'hoverTime', new Date().getTime() );
-    });
-    li.children('a').click(function(){
-      // only allow click if at least 50ms has elapsed since hover
-      return ( new Date().getTime() - li.data('hoverTime') ) > 50;
-    });
-  });
-}
